594640312e7982bb58093d236c7ab30fb9bd1430
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2025-01-21 19:12:55 +0600

    21-1-25

--- views/admin_partials/head.php
+++ views/admin_partials/head.php
@@ -33,8 +33,8 @@
     type="text/css" />
 
   <!-- App css -->
-  <link href="<?= ROOT_URL ?>public/admin/css/app.min.css?v=<?= filemtime('public/admin/css/app.min.css') ?>"
-    rel="stylesheet" type="text/css" id="app-style" />
+  <link
+    href="<?= ROOT_URL ?>public/admin/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
 
   <!-- Icons -->
   <link href="<?= ROOT_URL ?>public/admin/css/icons.min.css" rel="stylesheet" type="text/css" />
