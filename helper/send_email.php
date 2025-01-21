aaac9acfb7d47e29e9c2f0a424aa497760a287e1
shahed141123 <khandkershahed23@gmail.com>
2025-01-21 15:27:49 +0600

    30-12-2024

--- helper/send_email.php
+++ helper/send_email.php
@@ -22,17 +22,17 @@ function sendEmailToDonor($recipientEmail, $recipientName, $requesterName, $requ
         ];
         // $mail->Host = 'smtp-relay.brevo.com'; // Gmail SMTP server
         // $mail->SMTPAuth = true;
-        // $mail->Username = '83f8c2001@smtp-brevo.com'; // Your Gmail address
-        // $mail->Password = 'sHvMPqD9jWtAxa2g'; // App password generated from Google
-        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // TLS encryption
+        // $mail->Username = '83f8c2001@smtp-brevo.com'; 
+        // $mail->Password = 'sHvMPqD9jWtAxa2g';
+        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         // $mail->Port = 587; // Port for TLS encryption (587)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Host = 'smtp.gmail.com';
-        $mail->Port = 465;
+        $mail->Port = 587;
         $mail->SMTPAuth = true;
-        $mail->Username = 'dev1.ngenit@gmail.com'; // Your Gmail address
-        $mail->Password = 'nhpptnbuwvcuyrtf'; // App password generated from Google
-        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // TLS encryption
+        $mail->Username = 'dev1.ngenit@gmail.com'; 
+        $mail->Password = 'nhpptnbuwvcuyrtf'; 
+        // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         // $mail->Port = 465; // Port for TLS encryption (587)
 
         // Sender and recipient information
